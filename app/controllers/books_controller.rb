8ba93fcf6c7f76b26437b2b408af0e0e6d16d9f1
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-09-17 07:36:34 +0000

    [Update] routingとheaderの各種リンク

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+
+  def destroy
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end 
+
+  def edit
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+  
 end
