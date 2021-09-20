ad48381c1a900b323a21c6a4d0c92519a78ee091
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-09-20 06:19:52 +0000

    [Update] books/index.html.erbのBooks一覧

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def index
     @user = current_user
     @book = Book.new
+    @books = Book.all
   end
   
   def show
